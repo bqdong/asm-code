3bbdf9abcd46c041806e13e04c3a8135fb99e826
bqdong <bqdong.dev@gmail.com>
2024-10-15 23:30:15 +0800

    init

---
+++ basics/main.s
@@ -0,0 +1,28 @@
+/*
+GUN Assembler
+
+Command Line Example:
+
+```bash
+as -o my-object-file.o my-asm.s
+as -omy-object-file.o my-asm.s
+```
+
+Conventionally, for input file, suffix is .s, represent source file.
+For output file, suffix is .o, represent oject file.
+
+# Syntax
+
+## Preprocessing
+
+## Whitespace
+
+## Comments
+
+## Symbols
+
+## Statements
+
+## Constants
+
+*/
